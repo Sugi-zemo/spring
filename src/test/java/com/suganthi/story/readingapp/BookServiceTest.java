96340ca89a5d345ec3c37a0936c4b41df43b5fdc
Sugi-zemo <102783438+Sugi-zemo@users.noreply.github.com>
2022-05-27 13:20:41 +0530

    Update BookServiceTest.java

--- src/test/java/com/suganthi/story/readingapp/BookServiceTest.java
+++ src/test/java/com/suganthi/story/readingapp/BookServiceTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.ui.Model;
 
@@ -23,7 +24,7 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class BookServiceTest {
 
 
